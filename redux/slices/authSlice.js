f4b4750e0a816ef4f37be1f47734de72c3f5df4f
Vishal <Vishal@MJBPUNL048.MJBTECH.COM>
2024-06-20 07:49:36 +0530

    enn variable

--- redux/slices/authSlice.js
+++ redux/slices/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = process.env.NEXT_PUBLIC_API_URL
 
 // Function to store token in localStorage
 const storeToken = (token) => {
--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getToken } from "./authToken";
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
